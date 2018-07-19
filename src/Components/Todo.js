dd0768e071e3bba28bb77fc4aae053dc8bedc28b
cirnobreak <1103054869@qq.com>
2018-07-19 23:54:05 +0800

    add styled-jsx and antd

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Todo = ({ content }) => (
   <li>
     <a href="/#">{ content }</a>
+    <style jsx>{`
+      a {
+        text-decoration: none;
+      }
+    `}</style>
   </li>
+  
 )
 
 export default Todo
